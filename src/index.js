6d3d79eab91ed878c8f97368891cce592b4c9023
Tyler <brown.tyler.w@gmail.com>
2024-01-07 21:39:47 -0800

    Structured page Added button that displays form to add project name

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import './style.css';
 import loadUI from './ui';
 
 document.body.appendChild(loadUI());
+const newBtn = document.getElementById('new-btn');
+newBtn.addEventListener('click', loadProjectForm);
+
+function loadProjectForm() {
+    const proj = document.getElementById('project-form');
+    if (proj.style.display == 'flex') {
+        proj.style.display = 'none';
+    } else {
+        proj.style.display = 'flex';
+    }
+}
--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,27 @@
-import './style.css';
+
 
 function loadUI() {
     const container = document.createElement('div');
     container.classList.add('container');
-    container.innerHTML = 'To-Do-List';
+
+    const sidebar = document.createElement('div');
+    sidebar.classList.add('sidebar');
+    sidebar.innerHTML = 'To - Do - List';
+
+    const new_project = document.createElement('button');
+    new_project.setAttribute('id', 'new-btn')
+    new_project.innerHTML = '+ New';
+
+    const project_form = document.createElement('div');
+    project_form.classList.add('project-form');
+    project_form.setAttribute('id', 'project-form');
+    const name = document.createElement('input');
+    name.placeholder = 'Project Name';
+    project_form.appendChild(name);
+
+    sidebar.append(new_project);
+    sidebar.append(project_form);
+    container.appendChild(sidebar);
     return container;
 }
 
